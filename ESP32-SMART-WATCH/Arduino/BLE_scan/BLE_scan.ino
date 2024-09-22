e72bf4acca953f793a838f69ea690471ac2cbea3
paycheckpacheck <pacheck@vt.edu>
2024-09-22 16:23:23 -0400

    Updated BLDC v5, Ready to Order, Need to add Caps to the BOM

--- ESP32-SMART-WATCH/Arduino/BLE_scan/BLE_scan.ino
+++ ESP32-SMART-WATCH/Arduino/BLE_scan/BLE_scan.ino
@@ -1,7 +1,3 @@
-/*
-   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
-   Ported to Arduino ESP32 by Evandro Copercini
-*/
 
 #include <BLEDevice.h>
 #include <BLEUtils.h>
